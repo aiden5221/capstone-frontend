9f4f3c65f97c8785ab2849d5e73e26c3763af935
Aiden Jolley-Ruhn <72046405+aiden5221@users.noreply.github.com>
2023-01-30 21:00:32 -0500

    final update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,18 @@ import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
 import { Stack } from '@mui/system'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { NAV_HEADINGS } from './constants'
 
 function Footer({ isLoggedIn }) {
 
     const TextStyling = {
         transition: 'color 0.5s ease-out',
-        fontFamily:"'Lato', sans-serif;",
+        fontFamily: "'Lato', sans-serif;",
         '&:hover': {
             color: "white",
-        }
+        },
+        whiteSpace: 'nowrap',
+        fontSize: '15px'
     }
 
     const LinkStyling = {
@@ -19,98 +22,81 @@ function Footer({ isLoggedIn }) {
         textDecoration:'none',
     }
 
-  return (
-    <Grid2
-        container
-        sx={{ position: 'fixed', width:'100%', display:'flex', left:0, bottom:30, right:0,  borderTop:'1px solid' }}>
-            <Grid2
-                item
-                xsOffset={1}
-                xs={10}
-                sx={{display:'flex'}}>
-                <Typography
-                    sx={{ position:'fixed', right:'20%', bottom:0, fontFamily:"'Lato', sans-serif;" }}>
-                    &#169; 2023 Job Board
-                </Typography>
+    const checkIfLoggedIn = (heading) => heading == 'Login' && isLoggedIn || heading == 'Logout' && !isLoggedIn
+
+    return (
+        <Grid2
+            container
+            sx={{ position: 'fixed', width:'100%', display:'flex', left:0, bottom:30, right:0,  borderTop:'1px solid' }}>
                 <Grid2
                     item
-                    xs={1}>
-                    <Stack
-                        sx={{ marginTop:1 }}
-                        spacing={1}>
-                        <Link
-                            style={ LinkStyling }
-                            to={'/home'}>
-                            <Typography
-                                sx={ TextStyling }>
-                                Home
-                            </Typography>
-                        </Link>
-                        <Link
-                            style={ LinkStyling }
-                            to={'/postings'}>
-                            <Typography
-                                sx={ TextStyling }>
-                                Postings
-                            </Typography>
-                        </Link>
-                        {
-                            isLoggedIn ?
-                            <Link
-                                style={ LinkStyling }
-                                to={'/logout'}>
-                                <Typography
-                                    sx={ TextStyling }>
-                                    Logout
-                                </Typography>
-                            </Link>
-                            :
-                            <Link
-                                style={ LinkStyling }
-                                to={'/login'}>
-                                <Typography
-                                    sx={ TextStyling }>
-                                    Login
-                                </Typography>
-                            </Link>
-                        }
-                        
-                        
-                    </Stack>
-                </Grid2>
-                {
-                    // Provides logged in features such as creating posting and viewing your postings
-                    isLoggedIn &&
-                        <Grid2>
-                            <Stack
-                                sx={{ marginTop:1 }}
-                                spacing={1}>
-                                <Link
-                                    style={LinkStyling}
-                                    to={'/createPosting'}>
-                                    <Typography
-                                        sx={TextStyling}>
-                                        Create a posting
-                                    </Typography>
-                                </Link>
-                                <Link
-                                    style={LinkStyling}
-                                    to={'/yourPostings'}>
-                                    <Typography
-                                        sx={TextStyling}>
-                                        View your postings
-                                    </Typography>
-                                </Link>
+                    xsOffset={1}
+                    xs={10}
+                    sx={{display:'flex'}}>
+                    <Typography
+                        sx={{ position:'fixed', right:'20%', bottom:0, fontFamily:"'Lato', sans-serif;" }}>
+                        &#169; 2023 Job Board
+                    </Typography>
+                    <Grid2
+                        item
+                        xs={1}>
+                        <Stack
+                            sx={{ marginTop:1 }}
+                            spacing={1}>
+                                {
+                                
+                                    NAV_HEADINGS.map((heading) => {
 
-                            </Stack>
-                        </Grid2>
-                }
-                
-               
-                
-            </Grid2>
-    </Grid2>
-  )
+                                        if(checkIfLoggedIn(heading)) return null
+
+                                        return (
+                                            <Link
+                                                to={heading}
+                                                style={ LinkStyling }>
+                                                <Typography
+                                                    variant='h5'
+                                                    sx={ TextStyling }>
+                                                    {heading}
+                                                </Typography>
+                                            </Link>
+                                        )
+                                    })
+                                
+                                }
+                            
+                        </Stack>
+                    </Grid2>
+                    {
+                        // Provides logged in features such as creating posting and viewing your postings
+                        isLoggedIn &&
+                            <Grid2>
+                                <Stack
+                                    sx={{ marginTop:1 }}
+                                    spacing={1}>
+                                    <Link
+                                        style={LinkStyling}
+                                        to={'/createPosting'}>
+                                        <Typography
+                                            sx={TextStyling}>
+                                            Create a posting
+                                        </Typography>
+                                    </Link>
+                                    <Link
+                                        style={LinkStyling}
+                                        to={'/yourPostings'}>
+                                        <Typography
+                                            sx={TextStyling}>
+                                            View your postings
+                                        </Typography>
+                                    </Link>
+
+                                </Stack>
+                            </Grid2>
+                    }
+                    
+                </Grid2>
+        </Grid2>
+    )
 }
 
 export default Footer
--- src/components/Navbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar.jsx
@@ -3,6 +3,7 @@ import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
 import React, { useState } from 'react'
 import { FaBars } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { NAV_HEADINGS } from '../constants'
 
 
 function MobileNavbar({ isLoggedIn }) {
@@ -28,8 +29,6 @@ function MobileNavbar({ isLoggedIn }) {
         
     }
 
-    const HEADINGS = [ 'Home', 'Postings', 'Login', 'Logout' ]
-
     const checkIfLoggedIn = (heading) => heading == 'Login' && isLoggedIn || heading == 'Logout' && !isLoggedIn
     
     return (
@@ -44,7 +43,7 @@ function MobileNavbar({ isLoggedIn }) {
                     <List
                         sx={{ width:'50vw', height:'100vh', backgroundColor:'#5f4c4c' }}>
                             {
-                                HEADINGS.map((heading) => {
+                                NAV_HEADINGS.map((heading) => {
                                     if(checkIfLoggedIn(heading)) return null
                                     return(
                                     <ListItem
