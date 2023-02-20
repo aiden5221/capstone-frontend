72e279ea0390a789aa2996abe37915fba27b29ee
adhishmalhotra <adhish.malhotra@ontariotechu.net>
2023-02-20 12:40:00 -0500

    Finishing up some experimental changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Homepage from './components/Pages/Homepage/Homepage'
 import NavbarLayout from './components/Layouts/NavbarLayout'
+import CreateApplicationForm from './components/CreateApplicationForm/CreateApplicationForm'
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <Route path='/' element={<NavbarLayout />}>
           <Route index path='home' element={<Homepage />}/>
           <Route path='postings' element={null} />
-          <Route path='createAPosting' element={null} />
+          <Route path='createAPosting' element={<CreateApplicationForm />} />
           <Route path='login' element={null} />
           <Route path='signup' element={null} />
       </Route>
--- src/components/CreateApplicationForm/CreateApplicationForm.jsx
+++ src/components/CreateApplicationForm/CreateApplicationForm.jsx
@@ -2,9 +2,100 @@ import React from 'react'
 import { Button, CardContent, Grid, TextareaAutosize, TextField, Typography } from '@mui/material'
 import {Card} from '@mui/material'
 import { textAlign } from '@mui/system'
-function CreateApplicationForm() {
+import { makeStyles } from '@mui/styles'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Input from '@mui/material/Input';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+
+// Icons
+import EditIcon from '@mui/icons-material/Edit';
+import DoneIcon from '@mui/icons-material/Done';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+
+const createData = (skillname, weightedValue) => ({
+  id: skillname.replace(" ", "_"),
+  skillname,
+  weightedValue,
+  isEditMode: false
+});
+
+const CustomTableCell = ({ row, name, onChange }) => {
+  const { isEditMode } = row;
   return (
-    <div>
+    <TableCell align="left">
+      {isEditMode ? (
+        <Input
+          value={row[name]}
+          name={name}
+          onChange={e => onChange(e, row)}
+        />
+      ) : (
+        row[name]
+      )}
+    </TableCell>
+  );
+};
+
+
+function CreateApplicationForm() {
+
+  const [rows, setRows] = React.useState([
+    createData("Java", 8),
+    createData("Python", 7),
+  ]);
+
+  const [previous, setPrevious] = React.useState({});
+
+  const onToggleEditMode = id => {
+    setRows(state => {
+      return rows.map(row => {
+        if (row.id === id) {
+          return { ...row, isEditMode: !row.isEditMode };
+        }
+        return row;
+      });
+    });
+  };
+
+  const onChange = (e, row) => {
+    if (!previous[row.id]) {
+      setPrevious(state => ({ ...state, [row.id]: row }));
+    }
+    const value = e.target.value;
+    const name = e.target.name;
+    const { id } = row;
+    const newRows = rows.map(row => {
+      if (row.id === id) {
+        return { ...row, [name]: value };
+      }
+      return row;
+    });
+    setRows(newRows);
+  };
+
+  const onRevert = id => {
+    const newRows = rows.map(row => {
+      if (row.id === id) {
+        return previous[id] ? previous[id] : row;
+      }
+      return row;
+    });
+    setRows(newRows);
+    setPrevious(state => {
+      delete state[id];
+      return state;
+    });
+    onToggleEditMode(id);
+  };
+
+  return (
+    <Paper style={{overflow: "hidden", marginBottom: "5px"}}>
       <Typography variant='h4' gutterBottom marginTop={2} marginLeft={2} style={{textAlign:'center'}}>
         Create Job Application Form
       </Typography>
@@ -32,6 +123,58 @@ function CreateApplicationForm() {
                 <TextField label = "Job Qualifications" placeholder='Enter Job Qualifications here' variant='outlined' multiline fullWidth rows={4}>
                 </TextField>
               </Grid>
+  
+              <Table>
+                <caption>Weighted Skills</caption>
+                <TableHead>
+                  <TableRow>
+                  <Button
+                    type='submit'
+                    variant='contained'
+                    color='primary'
+                    style={{ width: "10%" }}
+                  >
+                    Add
+                  </Button>
+                  <TableCell align="left">Skill Name</TableCell>
+                  <TableCell align="left">Weighted Value</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {rows.map(row => (
+                    <TableRow key={row.id}>
+                      <TableCell>
+                        {row.isEditMode ? (
+                          <>
+                            <IconButton
+                              aria-label="done"
+                              onClick={() => onToggleEditMode(row.id)}
+                            >
+                              <DoneIcon />
+                            </IconButton>
+                            <IconButton
+                              aria-label="revert"
+                              onClick={() => onRevert(row.id)}
+                            >
+                              <CancelIcon />
+                            </IconButton>
+                          </>
+                        ) : (
+                          <IconButton
+                            aria-label="delete"
+                            onClick={() => onToggleEditMode(row.id)}
+                          >
+                            <EditIcon />
+                          </IconButton>
+                        )}
+                      </TableCell>
+                      <CustomTableCell {...{ row, name: "skillname", onChange }} />
+                      <CustomTableCell {...{ row, name: "weightedValue", onChange }} />
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
               <Grid xs = {12} item>
                 <TextField type = "date"  placeholder='Enter Application Deadline here' variant='outlined' fullWidth>
                 </TextField>
@@ -45,7 +188,7 @@ function CreateApplicationForm() {
           </form>
         </CardContent>
       </Card>
-    </div>
+    </Paper>
   )
 }
 
